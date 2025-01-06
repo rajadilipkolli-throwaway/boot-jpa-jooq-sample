53e586f5f452793086bfbcdda258a7bd16f8ae17
Raja Kolli <rajadilipkolli@gmail.com>
2025-01-06 17:02:10 +0000

    fix : spotless formatting issue

--- src/main/java/com/example/learning/model/request/PostRequest.java
+++ src/main/java/com/example/learning/model/request/PostRequest.java
@@ -2,11 +2,4 @@ package com.example.learning.model.request;
 
 import java.time.LocalDateTime;
 
-public record PostRequest(String title,
-
-                          String content,
-
-                          boolean published,
-
-                          LocalDateTime publishedAt) {
-}
+public record PostRequest(String title, String content, boolean published, LocalDateTime publishedAt) {}
